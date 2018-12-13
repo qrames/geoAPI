7101885293156cc8ce49b1a26d23aecef568f20a
qrames <ramque@orange.fr>
2018-12-13 11:08:14 +0100

    view : GeoPointViewSet

--- map/views.py
+++ map/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import GeoPointSerializer
+from .models import GeoPoint
+
+
+class GeoPointViewSet(viewsets.ModelViewSet):
+    queryset = GeoPoint.objects.all()
+    serializer_class = GeoPointSerializer
