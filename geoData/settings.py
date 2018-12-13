ad13027f08e0b1677610637216a26d88b19d0dd4
qrames <ramque@orange.fr>
2018-12-13 10:35:36 +0100

    model admin with LeafletGeoAdmin

--- geoData/settings.py
+++ geoData/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',    
+
+    # dependency:
+    'leaflet',
+    # app :
+    'map',
 ]
 
 MIDDLEWARE = [
--- map/admin.py
+++ map/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import GeoPoint
+from leaflet.admin import LeafletGeoAdmin
 # Register your models here.
+
+
+
+admin.site.register(GeoPoint, LeafletGeoAdmin)
