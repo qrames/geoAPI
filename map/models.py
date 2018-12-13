83f2a3153b7532ed8c16e8dc65a436fbf9eb7e93
qrames <ramque@orange.fr>
2018-12-13 10:32:20 +0100

    GeoPoint model

--- map/models.py
+++ map/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
-
+from django.contrib.gis.db import models
 # Create your models here.
+
+class GeoPoint(models.Model):
+    name = models.CharField(max_length=200)
+    geom = models.GeometryField()
